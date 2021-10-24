d81501ebb3e396481c590858c52e20d146d81dae
s1nisteR <ankon1441@gmail.com>
2021-10-24 17:14:10 +0600

    Added new problemss

---
+++ Codeforces/Expression/file.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <iomanip>
+#include <string.h>
+#include <stdlib.h>
+#include <unordered_map>
+#include <assert.h>
+
+
+using namespace std;
+
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int temp = a + (b * c);
+    int temp2 = a * (b + c);
+    int temp3 = a * b * c;
+    int temp4 = (a + b) * c;
+    int temp5 = a + b + c;
+    cout << max(temp, max(temp2, max(temp3, max(temp4, temp5)))) << "\n";
+
+
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputf.in","r",stdin);
+    freopen("outputf.in","w",stdout);
+#endif
+
+    //Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+/*
+    long long t;
+    cin >> t;
+    for(long long i = 0; i < t; ++i)
+    {
+        solve();
+    }
+*/
+
+    solve();
+
+
+    return 0;
+}
---
+++ Codeforces/Presents/file.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <iomanip>
+#include <string.h>
+#include <stdlib.h>
+#include <unordered_map>
+#include <assert.h>
+
+
+using namespace std;
+
+
+void solve()
+{
+    int n;
+    int inp;
+    cin >> n;
+    vector<pair<int, int> > arr;
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> inp;
+        arr.push_back(make_pair(inp, i + 1));
+    }
+    sort(arr.begin(), arr.end());
+    for(auto& x : arr)
+    {
+        cout << x.second << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputf.in","r",stdin);
+    freopen("outputf.in","w",stdout);
+#endif
+
+    //Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+/*
+    long long t;
+    cin >> t;
+    for(long long i = 0; i < t; ++i)
+    {
+        solve();
+    }
+*/
+
+    solve();
+
+
+    return 0;
+}
