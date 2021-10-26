db6e89ec73d10aafdf394f474707ad74abb1f89a
s1nisteR <ankon1441@gmail.com>
2021-10-26 19:07:59 +0600

    Added new problems

---
+++ UVa/10071 - Back to High School Physics/file4.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <iomanip>
+#include <string.h>
+#include <stdlib.h>
+#include <unordered_map>
+#include <assert.h>
+
+using namespace std;
+
+
+void solve()
+{
+    int v1, t1;
+	while(scanf("%d %d", &v1, &t1) != EOF){
+		printf("%d\n",2*v1*t1);
+	}
+
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputf.in","r",stdin);
+    freopen("outputf.in","w",stdout);
+#endif
+
+    //Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+/*
+    long long t;
+    cin >> t;
+    for(long long i = 0; i < t; ++i)
+    {
+        solve();
+    }
+*/
+
+    solve();
+
+
+    return 0;
+}
---
+++ UVa/272 - TEX Quotes/file4.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <iomanip>
+#include <string.h>
+#include <stdlib.h>
+#include <unordered_map>
+#include <assert.h>
+
+using namespace std;
+
+
+void solve()
+{
+    string inp;
+    string ans = "";
+    bool alternator = true;
+    while(getline(cin, inp))
+    {   
+        for(auto& x : inp)
+        {
+            if(x == '"')
+            {
+                if(alternator)
+                {
+                    ans += "``";
+                    alternator = false;
+                }
+                else
+                {
+                    ans += "''";
+                    alternator = true;
+                }
+            }
+            else
+            {
+                ans += x;
+            }
+        }
+        cout << ans << "\n";
+        ans = "";
+    }    
+
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("inputf.in","r",stdin);
+    freopen("outputf.in","w",stdout);
+#endif
+
+    //Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+/*
+    long long t;
+    cin >> t;
+    for(long long i = 0; i < t; ++i)
+    {
+        solve();
+    }
+*/
+
+    solve();
+
+
+    return 0;
+}
